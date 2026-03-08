6a1d0ac9aa0b8e419d2db40bfd03a9313895b9c3
httpsdave <davedominic912@gmail.com>
2026-03-08 19:52:33 +0800

    revert to standard Firebase authDomain flow

--- next.config.ts
+++ next.config.ts
@@ -1,14 +1,7 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  async rewrites() {
-    return [
-      {
-        source: '/__/auth/:path*',
-        destination: 'https://gameofthegenerals-c67a9.firebaseapp.com/__/auth/:path*',
-      },
-    ];
-  },
+  /* config options here */
 };
 
 export default nextConfig;
